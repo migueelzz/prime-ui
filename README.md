41d8ef072c1c8a6e6a057253e9476bca92990a5d
Miguel Lemes <miguellemes005@gmail.com>
2025-02-20 10:21:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prime UI
+
+## Changelog (manual)
+
+- 0.0.3 - add TailwindCSS and Storybook
+- 0.0.2
+- 0.0.1
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		// reference the library only
+		'./lib/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#F58220',
+        'primary-dark': '#F26E22',
+				'blue-light': '#1E4870',
+				'blue-base': '#031C33',
+				'blue-dark': '#001528',
+			}
+		},
+	},
+	plugins: [],
+}
+
